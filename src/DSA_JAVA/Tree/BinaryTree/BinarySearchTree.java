9c9f5fccf4ec3388b13988b9ce64b304cdaf4a97
Abhay <abhayganvir03@gmail.com>
2023-03-22 08:21:28 +0530

    Binary Search Tree in java

---
+++ src/DSA_JAVA/Tree/BinaryTree/BinarySearchTree.java
@@ -0,0 +1,55 @@
+package DSA_JAVA.Tree.BinaryTree;
+
+import com.sun.source.tree.Tree;
+
+public class BinarySearchTree {
+
+    private TreeNode root;
+
+    private class TreeNode{
+        private int data;
+        private TreeNode left;
+        private TreeNode right;
+
+        public TreeNode(int data){
+            this.data = data;
+        }
+    }
+
+    public TreeNode insert(TreeNode root,int value){
+        if (root == null){
+            root = new TreeNode(value);
+            return root;
+        }
+
+        if (value < root.data){
+            root.left = insert(root.left,value);
+        }
+        else{
+            root.right = insert(root.right, value);
+        }
+        return root;
+    }
+
+    public void InOrder(TreeNode root){
+        if (root == null){
+            return;
+        }
+        InOrder(root.left);
+        System.out.print(root.data + " ");
+        InOrder(root.right);
+
+    }
+
+    public static void main(String[] args) {
+        BinarySearchTree bt = new BinarySearchTree();
+        bt.root = bt.insert(bt.root, 10);
+        bt.insert(bt.root,-12);
+        bt.insert(bt.root,43);
+        bt.insert(bt.root, 123);
+
+        bt.InOrder(bt.root);
+
+
+    }
+}
