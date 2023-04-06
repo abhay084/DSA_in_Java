8c351f9c7c9ee3e12c221c2a89cf61cc34a8871a
Abhay <abhayganvir03@gmail.com>
2023-04-06 22:17:30 +0530

    insert at end of Doubly LL

--- src/DSA_JAVA/StriverA2Z_Sheet/BasicMaths/CountDigits.java
+++ src/DSA_JAVA/StriverA2Z_Sheet/BasicMaths/CountDigits.java
@@ -8,6 +8,17 @@ public class CountDigits {
         System.out.println(s.length());
     }
 
+    public int totalNUmbers(int N){
+        int count=0;
+        int num = N;
+
+        while (num > 0){
+            num = num/10;
+            count++;
+        }
+
+        return count;
+    }
 
     public static void main(String[] args) {
 
@@ -17,6 +28,10 @@ public class CountDigits {
 
         System.out.println("Number of Digits in a number " + N + " is : ");
         cd.IntTOString(N);
+        System.out.println();
+
+        int cnt = cd.totalNUmbers(N);
+        System.out.println(cnt);
 
 
 
